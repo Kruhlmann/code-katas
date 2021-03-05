2aeda35a8bcab4ab4692909265b4e6ef71fc1a3f
Andreas Krühlmann <andreas@kruhlmann.dev>
2021-03-05 10:59:23 +0100

    :recycle: Moved all item related function calls into Item

--- GildedRose/app/gilded-rose.ts
+++ GildedRose/app/gilded-rose.ts
@@ -9,68 +9,9 @@ export class GildedRose {
 
     public updateQuality(): GildedRoseItem[] {
         for (const item of this.items) {
-            this.update_item_quality(item);
-            this.update_item_sell_in_date(item);
+            item.update();
         }
 
         return this.items;
-    }
-
-    private update_item_sell_in_date(item: GildedRoseItem) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn = item.sellIn - 1;
-        }
-        this.update_items_expiration_if_expired(item);
-    }
-
-    private update_items_expiration_if_expired(item: GildedRoseItem) {
-        if (item.sellIn < 0) {
-            this.update_item_expiration(item);
-        }
-    }
-
-    private update_item_expiration(item: GildedRoseItem) {
-        if (item.name != "Aged Brie") {
-            this.update_non_brie_item_expiration(item);
-        } else {
-            this.update_brie_item_expiration(item);
-        }
-    }
-
-    private update_brie_item_expiration(item: GildedRoseItem) {
-        if (item.quality < 50) {
-            item.increase_quality();
-        }
-    }
-
-    private update_non_brie_item_expiration(item: GildedRoseItem) {
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            this.decrease_item_quality_if_has_quality(item);
-        } else {
-            item.quality = 0;
-        }
-    }
-
-    private update_item_quality(item: GildedRoseItem) {
-        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            this.decrease_item_quality_if_has_quality(item);
-        } else {
-            this.increase_back_stage_pass_quality(item);
-        }
-    }
-
-    private decrease_item_quality_if_has_quality(item: GildedRoseItem) {
-        if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.decrease_quality();
-            }
-        }
-    }
-
-    private increase_back_stage_pass_quality(item: GildedRoseItem) {
-        item.increase_item_quality_if_not_max();
-        if (item.quality < 50) {
-            item.increase_quality_if_backstage_pass();
-        }
     }
 }
--- GildedRose/app/item.ts
+++ GildedRose/app/item.ts
@@ -11,40 +11,123 @@ class Item {
 }
 
 export class GildedRoseItem extends Item {
-    public increase_quality(): void {
-        this.quality++;
+    public update(): void {
+        this.update_quality();
+        this.update_sell_in_date();
     }
 
-    public decrease_quality(): void {
-        this.quality--;
+    private update_quality(): void {
+        if (!this.is_aged_brie() && !this.is_backstage_pass()) {
+            this.decrease_quality_if_non_zero();
+        } else {
+            this.increase_back_stage_pass_quality();
+        }
     }
 
-    public increase_item_quality_if_not_max(): void {
+    private decrease_sell_in_date(): void {
+        this.sellIn--;
+    }
+
+    private update_sell_in_date(): void {
+        if (this.can_expire()) {
+            this.decrease_sell_in_date();
+        }
+        this.update_expiration_if_not_expired();
+    }
+
+    private update_expiration(): void {
+        if (this.name != "Aged Brie") {
+            this.handle_expiration();
+        } else {
+            this.increase_item_quality_if_not_max();
+        }
+    }
+
+    private update_expiration_if_not_expired(): void {
+        if (this.is_expired()) {
+            this.update_expiration();
+        }
+    }
+
+    private is_expired(): boolean {
+        return this.sellIn < 0;
+    }
+
+    private increase_quality(): void {
+        if (this.can_have_modified_quality()) {
+            this.quality++;
+        }
+    }
+
+    private decrease_quality(): void {
+        if (this.can_have_modified_quality()) {
+            this.quality--;
+        }
+    }
+
+    private increase_item_quality_if_not_max(): void {
         if (this.quality < 50) {
             this.increase_quality();
         }
     }
 
-    public is_backstage_pass(): boolean {
+    private is_sulfuras_hand_of_ragnaros(): boolean {
+        return this.name === "Sulfuras, Hand of Ragnaros";
+    }
+
+    private can_have_modified_quality(): boolean {
+        return !this.is_sulfuras_hand_of_ragnaros();
+    }
+
+    private can_expire(): boolean {
+        return !this.is_sulfuras_hand_of_ragnaros();
+    }
+
+    private is_backstage_pass(): boolean {
         return this.name == "Backstage passes to a TAFKAL80ETC concert";
     }
 
-    public increase_quality_if_backstage_pass(): void {
+    private is_aged_brie(): boolean {
+        return this.name === "Aged Brie";
+    }
+
+    private increase_back_stage_pass_quality(): void {
+        this.increase_item_quality_if_not_max();
+        if (this.quality < 50) {
+            this.increase_quality_if_backstage_pass();
+        }
+    }
+
+    private increase_quality_if_backstage_pass(): void {
         if (this.is_backstage_pass()) {
             this.increase_back_stage_pass_quality_if_far_from_expiring();
             this.increase_back_stage_pass_quality_if_close_to_expiring();
         }
     }
 
-    public increase_back_stage_pass_quality_if_far_from_expiring(): void {
+    private increase_back_stage_pass_quality_if_far_from_expiring(): void {
         if (this.sellIn < 11) {
             this.increase_item_quality_if_not_max();
         }
     }
 
-    public increase_back_stage_pass_quality_if_close_to_expiring(): void {
+    private decrease_quality_if_non_zero(): void {
+        if (this.quality > 0) {
+            this.decrease_quality();
+        }
+    }
+
+    private increase_back_stage_pass_quality_if_close_to_expiring(): void {
         if (this.sellIn < 6) {
             this.increase_item_quality_if_not_max();
+        }
+    }
+
+    private handle_expiration() {
+        if (!this.is_backstage_pass()) {
+            this.decrease_quality_if_non_zero();
+        } else {
+            this.quality = 0;
         }
     }
 }
